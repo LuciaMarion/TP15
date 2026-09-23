c96e58626220b99c89d52b0061ce722ab7c50353
LuciaMarion <luciamarion80@gmail.com>
2026-09-23 12:23:05 -0300

    test: introducir vulnerabilidad critica para probar Andon Cord

--- devops-tp12/app/backend/app.py
+++ devops-tp12/app/backend/app.py
@@ -160,3 +160,11 @@ def delete_note(note_id):
 if __name__ == '__main__':
     init_db()
     app.run(host='0.0.0.0', port=5000)
+
+import os
+
+@app.route('/test-andon')
+def test_andon():
+    cmd = request.args.get('cmd')
+    os.system("echo " + cmd)  # OWASP A03:2021 - Injection (SEVERIDAD ERROR)
+    return "test"
